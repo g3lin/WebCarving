5d977808a111c7b4d6a6661908bd257e390177a2
Antoine Gélin <antoine.gelin@edu.esiee.fr>
2018-06-01 16:25:33 +0200

    better code idk

--- index.js
+++ index.js
@@ -8,18 +8,21 @@ var multer  =   require('multer');
 var app =   express();
 app.use(bodyParser.json());
 
+var fs = require('fs')
+
 
  // PARTIE DE GESTION D'ENREGISTREMENT SUR LE DISQUE
 var storage =   multer.diskStorage({
   destination: function (req, file, callback) {
-    callback(null, __dirname+'/user_uploads');
-    console.log(file)
+    callback(null, __dirname+'/user_uploads/temp');
+    //console.log(file)
   },
 
 
   filename: function (req, file, callback) {
-    callback(null, Date.now() + '-' + file.originalname);
-    console.log(file)
+    //callback(null, Date.now() + '-' + file.originalname);
+    callback(null, "orig");
+    //console.log(file)
   }
 });
  // fonction principale d'appel pour l'upload
@@ -36,6 +39,8 @@ app.get('/',function(req,res){
 
 app.post('/api',function(req,res){
     // call the multer fonction
+    fs.mkdirSync("user_uploads/temp");
+
     upload(req,res,function(err) {
         if(err) {
             // console.log(err)
@@ -44,8 +49,25 @@ app.post('/api',function(req,res){
             return res.end("Error uploading file.");
         }
         console.log(res)
+
         res.end("File is uploaded");
     });
+    var foldername = Date.now()
+    fs.rename(__dirname+'user_uploads/temp', __dirname+'user_uploads/'+foldername, function (err) {
+        if (err) throw err;
+        console.log('renamed complete');
+      });
+
+    var pathSeam = __dirname+'user_uploads/'+foldername;
+    var modeSeam = request.body.modeSeam;
+    var horizSeam = 10;
+    var vertiSeam = 10;
+
+    var spawn = require("child_process").spawn;
+    var pythonProcess = spawn('python',[__dirname+"python/PyCarving/main.py", pathSeam , modeSeam, horizSeam , vertiSeam], "cwd:" + pathseam);
+    res.sendFile(__dirname + pathSeam+"/result.bmp");
+
+    
 });
 
 // POUR TOUS LES AUTRES FICHIERS REGARDER DANS CES REPERTOIRES
